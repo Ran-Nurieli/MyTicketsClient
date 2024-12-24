665669e08a6683b4bd856d9d2e55cee53316cbf6
Ran-Nurieli <136595069+Ran-Nurieli@users.noreply.github.com>
2024-12-24 20:04:12 +0200

    fixed login info visibility and service call

--- MyTicketsClient/Models/LoginInfo.cs
+++ MyTicketsClient/Models/LoginInfo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MyTicketsClient.Models
 {
-    class LoginInfo
+    public class LoginInfo
     {
 
         public string Email { get; set; }
--- MyTicketsClient/ViewModels/LoginPageViewModel.cs
+++ MyTicketsClient/ViewModels/LoginPageViewModel.cs
@@ -100,32 +100,32 @@ public class LoginPageViewModel : ViewModelBase
 
     private async void OnLogin()
     {
-        ////Choose the way you want to blobk the page while indicating a server call
-        //InServerCall = true;
-        //ErrorMsg = "";
-        ////Call the server to login
-        //LoginInfo loginInfo = new LoginInfo { Email = Email, Password = Password };
-        //User? u = await this.proxy.LoginAsync(loginInfo);
+        //Choose the way you want to blobk the page while indicating a server call
+        InServerCall = true;
+        ErrorMsg = "";
+        //Call the server to login
+        LoginInfo loginInfo = new LoginInfo { Email = Email, Password = Password };
+        User? u = await this.proxy.LoginAsync(loginInfo);
 
-        //InServerCall = false;
+        InServerCall = false;
 
-        ////Set the application logged in user to be whatever user returned (null or real user)
-        //((App)Application.Current).LoggedInUser = u;
-        //if (u == null)
-        //{
-        //    ErrorMsg = "Invalid email or password";
-        //}
-        //else
-        //{
-        //    ErrorMsg = "";
-        //    //Navigate to the main page
-        //    AppShell shell = serviceProvider.GetService<AppShell>();
-        //    //TasksViewModel tasksViewModel = serviceProvider.GetService<TasksViewModel>();
-        //    //tasksViewModel.Refresh(); //Refresh data and user in the tasksview model as it is a singleton
-        //    //((App)Application.Current).MainPage = shell;
-        //    //Shell.Current.FlyoutIsPresented = false; //close the flyout
-        //    //Shell.Current.GoToAsync("Tasks"); //Navigate to the Tasks tab page
-        //}
+        //Set the application logged in user to be whatever user returned (null or real user)
+        ((App)Application.Current).LoggedInUser = u;
+        if (u == null)
+        {
+            ErrorMsg = "Invalid email or password";
+        }
+        else
+        {
+            ErrorMsg = "";
+            //Navigate to the main page
+            AppShell shell = serviceProvider.GetService<AppShell>();
+            //TasksViewModel tasksViewModel = serviceProvider.GetService<TasksViewModel>();
+            //tasksViewModel.Refresh(); //Refresh data and user in the tasksview model as it is a singleton
+            //((App)Application.Current).MainPage = shell;
+            //Shell.Current.FlyoutIsPresented = false; //close the flyout
+            //Shell.Current.GoToAsync("Tasks"); //Navigate to the Tasks tab page
+        }
     }
 
 
