bf617ca648732b42073d67280dd9ed8a360f6acb
User <User@LAB2-9>
2025-01-14 12:49:32 +0200

    added update user funcion o conroller

--- MyTicketsClient/Services/MyTicketServerClientApi.cs
+++ MyTicketsClient/Services/MyTicketServerClientApi.cs
@@ -179,7 +179,31 @@ namespace MyTicketsClient.Services
         }
 
 
-
+        public async Task<bool> UpdateUser(User user)
+        {
+            //Set URI to the specific function API
+            string url = $"{this.baseUrl}updateuser";
+            try
+            {
+                //Call the server API
+                string json = JsonSerializer.Serialize(user);
+                StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(url, content);
+                //Check status
+                if (response.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
 
 
 
