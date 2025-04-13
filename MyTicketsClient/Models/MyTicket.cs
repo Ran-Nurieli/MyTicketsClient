c7b8045e147e692d762036b14640eaeac0151ec2
Ran-Nurieli <136595069+Ran-Nurieli@users.noreply.github.com>
2025-04-13 12:10:38 +0300

    added purchase request feature

---
+++ MyTicketsClient/Models/MyTicket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MyTicketsClient.Models
+{
+    public class MyTicket
+    {
+        public int TicketId { get; set; }
+        public string Username { get; set; } = null!;
+        public string? TeamName { get; set; } = null!;
+        public string Place { get; set; } = null!;
+        public int? Row { get; set; }
+        public int? Seats { get; set; } = null!;
+        public DateTime Date { get; set; }
+        public int Price { get; set; }
+        public bool IsAccepted { get; set; } = false;
+        public string? BuyerUsername { get; set; } = null!;
+        public string? BuyerPhone { get; set; } = null!;
+
+    }
+}
---
+++ MyTicketsClient/Models/PurchaseRequestUpdate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTicketsClient.Models
+{
+    public class PurchaseRequestUpdate
+    {
+        public int TicketId { get; set; }
+        public bool IsAccepted { get; set; } = false;
+    }
+}
